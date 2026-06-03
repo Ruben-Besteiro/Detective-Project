5828f3d5cb01673ce3a242ce816b3fc8b2679919
Untar la Manteca <rbesteiro@proton.me>
2026-06-03 18:09:09 +0200

    Menú principal

---
+++ Assets/Scripts/UI/SceneButton.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SceneButton : MonoBehaviour
+{
+    [SerializeField] private SceneTransition transitionType;
+    [SerializeField] private bool loadingIcon;
+
+    public void LoadScene(string LevelName)
+    {
+        GameSceneManager.Instance.LoadScene(LevelName, transitionType, loadingIcon);
+    }
+}
